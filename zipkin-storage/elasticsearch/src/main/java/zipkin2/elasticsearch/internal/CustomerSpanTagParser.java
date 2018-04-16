9890ac75a8ecb1c57fa53082dc517d8c9a5e9d37
zhangjinpeng <zhangjinpeng@jcfc.cn>
2018-04-16 10:49:36 +0800

    增加zipkin中的对tags的解析

---
+++ zipkin-storage/elasticsearch/src/main/java/zipkin2/elasticsearch/internal/CustomerSpanTagParser.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright 2015-2018 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package zipkin2.elasticsearch.internal;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Created by zhangjinpeng on 2018/4/15.
+ */
+
+public class CustomerSpanTagParser implements SpanTagParser {
+
+  private static final Pattern XML_PATTERN = Pattern.compile("^([0-9]{6})(<)$");
+  private static final Pattern JSON_PATTERN = Pattern.compile("^([0-9]{6})(\\{)$");
+
+  @Override
+  public void parse(Map<String, String> tags) {
+    if("dubbo".equals(tags.get("component"))) {
+      //增加对span中tags下的args、result的解析
+      if (tags.containsKey("args")) {
+        int idx = tags.get("args").indexOf(",");
+        if(idx>0 || idx<=15) {
+          String args = tags.get("args").substring(idx+1);
+          if (args.charAt(0) == '{') {//json格式
+            parseTagsArgsByJson(tags, args);
+          }
+          else if(args.charAt(0) == '<'){
+            parseTagsArgsByXml(tags, args);
+          }
+          else {
+            String subArgs = args.substring(0,7);
+            if(XML_PATTERN.matcher(subArgs).matches()){
+              parseTagsArgsByXml(tags, args.substring(6));
+            }
+            else if(JSON_PATTERN.matcher(subArgs).matches()){
+              parseTagsArgsByJson(tags, args.substring(6));
+            }
+          }
+        }
+      }
+      if (tags.containsKey("result")) {
+        String result = tags.get("result");
+        if(result.charAt(0)=='<') {
+          parseTagsResultByXml(tags, result);
+        }
+        else if(result.charAt(0)=='{'){
+          parseTagsResultByJson(tags, result);
+        }
+      }
+    }
+  }
+  private void parseTagsArgsByXml(Map<String, String> tags, String args) {
+    Document doc = null;
+    try {
+      doc = DocumentHelper.parseText(args);
+    } catch (DocumentException e) {
+      e.printStackTrace();
+    }
+    if (doc != null) {
+      Element root = doc.getRootElement();
+      for (Iterator iterator = root.elementIterator(); iterator.hasNext(); ) {
+        Element e = (Element) iterator.next();
+        if("head".equals(e.getName())){
+          for (Iterator headIt = e.elementIterator(); headIt.hasNext(); ) {
+            Element node = (Element) headIt.next();
+            tags.put("args-" + node.getName(), node.getText());
+          }
+        }
+      }
+    }
+  }
+
+  private void parseTagsArgsByJson(Map<String, String> tags, String args) {
+    JSONObject resultJson = JSON.parseObject(args);
+    if(resultJson.containsKey("head")){
+      JSONObject head = resultJson.getJSONObject("head");
+      for (String key : head.keySet()) {
+        tags.put("args-" + key, head.getString(key));
+      }
+    }
+  }
+
+  private void parseTagsResultByJson(Map<String, String> tags, String result) {
+    JSONObject resultJson = JSON.parseObject(result);
+    if(resultJson.containsKey("head")){
+      JSONObject head = resultJson.getJSONObject("head");
+      for (String key : head.keySet()) {
+        tags.put("result-" + key, head.getString(key));
+      }
+    }
+  }
+
+  private void parseTagsResultByXml(Map<String, String> tags, String result) {
+    Document doc = null;
+    try {
+      doc = DocumentHelper.parseText(result);
+    } catch (DocumentException e) {
+      e.printStackTrace();
+    }
+    if (doc != null) {
+      Element root = doc.getRootElement();
+      for (Iterator iterator = root.elementIterator(); iterator.hasNext(); ) {
+        Element e = (Element) iterator.next();
+        if("head".equals(e.getName())){
+          for (Iterator headIt = e.elementIterator(); headIt.hasNext(); ) {
+            Element node = (Element) headIt.next();
+            tags.put("result-" + node.getName(), node.getText());
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ zipkin-storage/elasticsearch/src/test/java/zipkin2/elasticsearch/internal/SpanTagParserTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2015-2018 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package zipkin2.elasticsearch.internal;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhangjinpeng on 2018/4/15.
+ */
+
+public class SpanTagParserTest {
+  private final SpanTagParser spanTagParser = new CustomerSpanTagParser();
+
+  @Test
+  public void testParser(){
+    Map<String, String> tags = new HashMap<>();
+    tags.put("component", "dubbo");
+    tags.put("args", "A1060001,000993<?xml version=\"1.0\" encoding=\"utf-8\" ?><root><head><tranCode>A1060001</tranCode><serno>20180415084843154283</serno><tranDate>2018-04-15</tranDate><tranTime>08:48:43</tranTime><chlGrp>0016</chlGrp><bchCde>900000</bchCde><starecord></starecord><page></page><version>1.0</version></head><body><sysid>A101</sysid><custtyp>05</custtyp><lmttyp>10</lmttyp><lmtnature>02</lmtnature><cycind>Y</cycind><currency>CNY</currency><termtyp>M</termtyp><limit/><gurttyp/><lmtocctyp/><comments/><lmtsts/><mainusr/><mainbch/><crtusr/><crtbch/><list><itemlist><applseq>CT4287476151750684672</applseq><prdtyp>20</prdtyp><prdcde>5103</prdcde><prdname>晋情借</prdname><cycind>Y</cycind><currency>CNY</currency><total>200000.00</total><termtyp>M</termtyp><limit>99</limit><comments/><itemsts/></itemlist></list><custid>1000090207</custid><idtyp>20</idtyp><idno>500236198410162750</idno><custname>石宗星</custname><startdt>2018-04-15</startdt><totalamt>200000.00</totalamt><enddt>2999-01-01</enddt></body></root>");
+    tags.put("result", "<root><head><tradeCode>A1060001</tradeCode><successCode>E00000000000</successCode><errorMessage>交易成功</errorMessage></head><body/></root>");
+
+    spanTagParser.parse(tags);
+
+//    System.out.println(JSON.toJSONString(tags));
+  }
+
+}
